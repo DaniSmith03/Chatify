cdae97bb22cf16d0b5880c5524e75864b2729504
Laura Medina <lcmedina390@gmail.com>
2021-08-10 18:56:20 -0500

    hooked up mongdb

--- server.js
+++ server.js
@@ -14,8 +14,22 @@ const { Server } = require("socket.io");
 const io = new Server(server);
 
 //database setup
-// const { Sequilize } = require('sequelize');
-// const sequelize = new Sequilize('postgres://localhost:5432/chatify'); //connect to the database
+const mongoose = require('mongoose');
+// Mongo DB Connection 
+  mongoose.connect("mongodb+srv://admin:dcbackend2021@cluster0.8yp23.mongodb.net/chatifydb?retryWrites=true&w=majority",  {
+    dbName: "chatifydb",
+    user: "admin",
+    pass: "dcbackend2021",
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+    .then( (res) => console.log('Mongo DB connected'))
+    .catch((err) => console.log(err));
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log("Database connection works.")
+});
 
 //authentication
 const bcrypt = require('bcrypt'); // hash user passwords
