0640f29910f9ebd3f23ebff5256686775ae7a303
Dani Smith <danismith03@gmail.com>
2021-08-14 09:39:26 -0500

    nonsense

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ const session = require('express-session'); //stores variables to be used persis
 const methodOverride = require('method-override');
 initializePassport(
   passport,
-  email => users.find(user => user.email === email),
+  email => user.find(user => user.email === email),
   id => users.find(user => user.id === id)
   )
 
@@ -46,64 +46,64 @@ app.use(passport.initialize())
 app.use(passport.session()) 
 app.use(methodOverride('_method'))
 
-// app.get('/', checkAuthenticated, (req, res) => {
-//   res.render('index.ejs');
-// });
+app.get('/', checkAuthenticated, (req, res) => {
+  res.render('index.ejs');
+});
 
-// app.get('/login', checkNotAuthenticated, (req, res) => {
-//   res.render('login.ejs')
-// });
+app.get('/login', checkNotAuthenticated, (req, res) => {
+  res.render('login.ejs')
+});
 
-// app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
-//   successRedirect: '/',
-//   failureRedirect: '/login',
-//   failureFlash: true
-// }))
+app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/login',
+  failureFlash: true
+}))
 
 
-// app.get('/register', checkNotAuthenticated, (req, res) => {
-//   res.render('register.ejs');
-// });
+app.get('/register', checkNotAuthenticated, (req, res) => {
+  res.render('register.ejs');
+});
 
-// app.post('/register', checkNotAuthenticated, async (req, res) => {
-//   try {
-//     const hashedPW = await bcrypt.hashSync(req.body.password, 10)
-//     users.push({
-//       id: Date.now().toString(),
-//       name: req.body.name,
-//       email: req.body.email,
-//       password: hashedPW
-//     });
-//     res.redirect('/login'); //is all is well, redirect to login page
-//   } catch {
-//     res.redirect('/register'); //if something goes wrong redirect to register page
-//   }
-//   console.log(users);
-// });
+app.post('/register', checkNotAuthenticated, async (req, res) => {
+  try {
+    const hashedPW = await bcrypt.hashSync(req.body.password, 10)
+    users.push({
+      id: Date.now().toString(),
+      name: req.body.name,
+      email: req.body.email,
+      password: hashedPW
+    });
+    res.redirect('/login'); //is all is well, redirect to login page
+  } catch {
+    res.redirect('/register'); //if something goes wrong redirect to register page
+  }
+  
+});
 
-// app.delete('/logout', (req, res) => {
-//   req.logOut()
-//   res.redirect('/login')
-// })
+app.delete('/logout', (req, res) => {
+  req.logOut()
+  res.redirect('/login')
+})
 
-// function checkAuthenticated(req, res, next) {
-//   if (req.isAuthenticated()) {
-//     return next()
-//   }
-//   res.redirect('/login')
-// }
-// function checkNotAuthenticated(req, res, next) {
-//   if (req.isAuthenticated()) {
-//     return res.redirect('/')
-//   }
-//   next()
-// }
+function checkAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next()
+  }
+  res.redirect('/login')
+}
+function checkNotAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return res.redirect('/')
+  }
+  next()
+}
 
-// io.on('connection', (socket) => {
-//     socket.on('chat message', (msg) => {
-//       io.emit('chat message', msg);
-//     });
-//   });
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+      io.emit('chat message', msg);
+    });
+  });
 
 
 server.listen(3000, () => {
